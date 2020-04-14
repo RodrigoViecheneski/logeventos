8da562c06d59c8e44efab0de2e8ce0d99724f407
Rodrigo Viecheneski <rodrigoviecheneski@gmail.com>
2020-04-14 12:00:01 -0300

    commit finalizado

---
+++ historico.class.php
@@ -0,0 +1,16 @@
+<?php
+class Historico {
+	private $pdo;
+	public function __construct() {
+		$this->pdo = new PDO("mysql:dbname=logeventos;host=localhost","root", "root");
+	}
+	public function registrar($acao){
+		$ip = $_SERVER['REMOTE_ADDR'];
+
+		$sql = "INSERT INTO historico SET ip = :ip, data_acao = NOW(), acao = :acao";
+		$sql = $this->pdo->prepare($sql);
+		$sql->bindvalue(":ip", $ip);
+		$sql->bindValue(":acao", $acao);
+		$sql->execute();
+	}
+}
